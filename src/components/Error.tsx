23eae0a6c002519ce36dd3e363a2a687301dae74
gorkavillar <gorka@gorkavillar.com>
2023-03-02 19:07:15 +0100

    🏍️ Tema 08 - Preparación d router

--- src/components/Error.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Error = ({ children } : any) => {
-  return (
-    <div style={ { color: "blue", fontWeight: 800 } }>{children}</div>
-  )
-}
-
-export default Error
