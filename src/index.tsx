650e78e603e000fa58c326348d917c81418c2d6e
gorkavillar <gorka@gorkavillar.com>
2023-03-09 10:35:24 +0100

    🐭 Tema 09 - Empezando con llamadas a APIs

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./index.css"
-import routes from "routes"
+import App from "App"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
-const router = createBrowserRouter(routes)
 
 root.render(
     // <React.StrictMode>
-    // <App />
-    <RouterProvider router={router} />
+    <App />
     // </React.StrictMode>
 )
