ac0ee4f861a957612ac62008fccfc09982ea3c06
gorkavillar <gorka@gorkavillar.com>
2023-03-09 11:54:09 +0100

    ⚛️ Tema 10 - Preparación Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable */
-import PokeView from "views/PokeView"
 import "./App.css"
 
 function App() {
     return (
         <div className="App">
-            <PokeView />
         </div>
     )
 }
--- src/httpClients/index.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios"
-
-export const pokeInfo = axios.create({ baseURL: "https://pokeapi.co/api/v2/pokemon" })
-
-export const clientInfo = axios.create({ baseURL: "https://misclientes.com", method: "POST", headers: {} })
