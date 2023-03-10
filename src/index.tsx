b89a5a7a7a64a0b0cddcc8cd08bf0ee6d4852a3d
gorkavillar <gorka@gorkavillar.com>
2023-03-10 13:04:58 +0100

    💖 Tema 12 - Añadidos los web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "App"
+import reportWebVitals from "reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
@@ -10,3 +11,5 @@ root.render(
     <App />
     // </React.StrictMode>
 )
+
+reportWebVitals(console.log)
