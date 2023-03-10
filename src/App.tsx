0c277fb9059d1f7b877a3ef530cb536963e52050
gorkavillar <gorka@gorkavillar.com>
2023-03-10 10:07:44 +0100

    🎨 Tema 11 - Preparando el entorno

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 /* eslint-disable */
-import ContadorContextProvider from "contexts/ContadorContextProvider"
-import TareasContextProvider from "contexts/TareasContextProvider"
-import Contador from "views/Contador"
-import ListaTareas from "views/ListaTareas"
 import "./App.css"
 
 function App() {
     return (
         <div className="App">
-            <ContadorContextProvider>
-                <Contador />
-                <TareasContextProvider>
-                    <ListaTareas />
-                </TareasContextProvider>
-            </ContadorContextProvider>
+            App
         </div>
     )
 }
