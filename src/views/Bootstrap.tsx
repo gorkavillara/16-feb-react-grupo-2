58d355d1b47ec0cb593bdcc32484ddf3214e702e
gorkavillar <gorka@gorkavillar.com>
2023-03-10 10:22:38 +0100

    ®️ Tema 11 - Inicializando reactstrap

--- src/views/Bootstrap.tsx
+++ src/views/Bootstrap.tsx
@@ -4,22 +4,6 @@ const Bootstrap = () => {
     return (
         <div>
             <h3>Bootstrap</h3>
-            <button type="button" className="btn btn-primary">
-                Primary
-            </button>
-            <div className="card" style={{ width: "18rem" }}>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn btn-primary">
-                        Go somewhere
-                    </a>
-                </div>
-            </div>
         </div>
     )
 }
