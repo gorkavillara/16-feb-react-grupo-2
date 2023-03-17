30bc09ee98abc83720a9ba54fdaa926e7552fd3a
gorkavillar <gorka@gorkavillar.com>
2023-03-17 12:58:42 +0100

    📺 Tema 15 - Webpack y babel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const app = document.getElementById('root')
+
+const root = ReactDOM.createRoot(app)
+
+root.render(<App />)
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+
+module.exports = {
+    // entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    }
+}
