0773b3fe3f22fc5dba42b5edc9651a662d894131
gorkavillar <gorka@gorkavillar.com>
2023-03-17 13:56:04 +0100

    ⌨️ Despliegue proyecto final

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Concurso from './components/Concurso';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <h1>App React Contest</h1>
+      </nav>
+      <div>
+        <Concurso />
+      </div>
     </div>
   );
 }
---
+++ src/components/Concurso.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+const frase = "Las mujeres ya no lloran las mujeres programan";
+
+const Concurso = () => {
+  const [fraseCorrecta, setFraseCorrecta] = useState("")
+  const [fraseRestante, setFraseRestante] = useState(frase)
+  const [inicio, setInicio] = useState<Date | undefined>()
+
+  useEffect(() => {
+    if (fraseRestante === "" && inicio) {
+      const ahora = new Date()
+      alert(
+        `Has tardado: ${(ahora.getTime() - inicio.getTime()) / 1000} s`
+      )
+    }
+  }, [fraseRestante, inicio])
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const ultimaLetraIntroducida = event.target.value.at(-1)
+    const primeraLetraRestante = fraseRestante.at(0)
+
+    if (ultimaLetraIntroducida !== primeraLetraRestante) return
+    if (fraseCorrecta === "") {
+      setInicio(new Date())
+    }
+
+    setFraseCorrecta(prev => `${prev}${ultimaLetraIntroducida}`)
+    setFraseRestante(prev => prev.substring(1, prev.length))
+  }
+
+  return (
+    <div>
+      <p>
+        <span style={ { color: "green" } }>{fraseCorrecta}</span>
+        <span>{fraseRestante}</span>
+      </p>
+      <input type="text" value={fraseCorrecta} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default Concurso;
